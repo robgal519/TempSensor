b760747c99b95b00eb54e8c69823aad6d302a297
Robert Gałat <robgal519@gmail.com>
2019-10-22 00:18:20 +0200

    implemented usart logger

--- Utils/Log.h
+++ Utils/Log.h
@@ -1,6 +1,24 @@
 #ifndef LOG_H
 #define LOG_H
 
-#define LOG(message)
+#ifndef LOGGER_TARGET // default target
+#define LOGGER_TARGET NONE
+#endif
+
+#define LOG(...) logger_send_message(__VA_ARGS__)
+
+enum Log_supported_targets{
+	USART,
+	NONE
+};
+
+void logger_send_message(const char* format, ...);
+
+#if LOGGER_TARGET == USART
+#include "USART_Log.h" 
+#else
+void logger_send_message(const char* format, ...){}
+#endif
+
 
 #endif
---
+++ Utils/USART_Log.c
@@ -0,0 +1,47 @@
+#include "USART_Log.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <Driver_USART.h>
+
+ARM_DRIVER_USART* Logger_connection;
+static uint32_t UART_event;
+
+extern ARM_DRIVER_USART LOGGER_CONNECTION;
+
+static void callback(uint32_t event){
+UART_event|=event;
+}
+
+static char sendBuffer[255];
+
+static void UART_logger_init(ARM_DRIVER_USART *connection){
+	if(connection){
+	Logger_connection = connection;
+	Logger_connection->Initialize(&callback);
+	Logger_connection->PowerControl(ARM_POWER_FULL);
+	Logger_connection->Control(
+        ARM_USART_MODE_ASYNCHRONOUS | 
+        ARM_USART_DATA_BITS_8 | 
+        ARM_USART_PARITY_NONE | 
+        ARM_USART_STOP_BITS_1 , LOGGER_BAUDRATE
+    );
+	Logger_connection->Control (ARM_USART_CONTROL_TX, 1);
+	Logger_connection->Control (ARM_USART_CONTROL_RX, 1);
+	}
+}
+
+void logger_send_message(const char* format, ...){
+if(!Logger_connection){
+		UART_logger_init(&LOGGER_CONNECTION);
+	}
+
+	va_list args;
+	va_start (args, format);
+  vsnprintf(sendBuffer,255,format,args);
+	va_end(args);
+	
+	Logger_connection->Send(sendBuffer, sizeof(sendBuffer)/sizeof(sendBuffer[0]));
+	while(UART_event!= ARM_USART_EVENT_SEND_COMPLETE);
+	UART_event = 0;
+
+}
